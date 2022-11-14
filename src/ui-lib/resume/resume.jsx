b1199fb37555472e01678b8bf83659190845a327
Latch Jackn <latch.jack@gmail.com>
2022-11-14 18:48:22 +0000

    Stripping components into seperate files

--- src/ui-lib/resume/resume.jsx
+++ src/ui-lib/resume/resume.jsx
@@ -1,44 +1,27 @@
 import Image from 'next/image'
 
+import nccGroupLogo from '@/images/logos/ncc-group-logo.svg'
 import { Button } from '@/components/Button'
-import logoAirbnb from '@/images/logos/airbnb.svg'
-import logoFacebook from '@/images/logos/facebook.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
-import logoStarbucks from '@/images/logos/starbucks.svg'
 import { BriefcaseIcon, ArrowDownIcon } from '@/ui-lib/site-icons/site-icons'
 
 function Resume() {
   let resume = [
     {
-      company: 'Planetaria',
-      title: 'CEO',
-      logo: logoPlanetaria,
-      start: '2019',
+      company: 'NCC Group',
+      title: 'Software Engineer',
+      logo: nccGroupLogo,
+      start: '2022',
       end: {
         label: 'Present',
         dateTime: new Date().getFullYear(),
       },
     },
     {
-      company: 'Airbnb',
-      title: 'Product Designer',
-      logo: logoAirbnb,
-      start: '2014',
-      end: '2019',
-    },
-    {
-      company: 'Facebook',
-      title: 'iOS Software Engineer',
-      logo: logoFacebook,
-      start: '2011',
-      end: '2014',
-    },
-    {
-      company: 'Starbucks',
-      title: 'Shift Supervisor',
-      logo: logoStarbucks,
-      start: '2008',
-      end: '2011',
+      company: 'NCC Group',
+      title: 'Junior Software Engineer',
+      logo: nccGroupLogo,
+      start: '2020',
+      end: '2022',
     },
   ]
 
